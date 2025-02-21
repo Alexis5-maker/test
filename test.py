ba993a811430497255b6742109702a89f3561efa
Alexis <alexisarreola805@gmail.com>
2025-02-21 15:35:56 -0700

    codigo base calculadora

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-print("Hola Mundo")
+def main():
+    print("Calculadora Simple")
+    print("Seleccione una operación:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Salir")
+    
+    opcion = input("Ingrese el número de la operación deseada: ")
+    
+    if opcion == '5':
+        print("Saliendo de la calculadora...")
+    else:
+        print("Opción seleccionada: ", opcion)
 
-
-
-print(5)
+if __name__ == "__main__":
+    main()
